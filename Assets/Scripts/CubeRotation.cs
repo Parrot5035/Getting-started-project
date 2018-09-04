3aa214e6fb0f9ef14ecda1a6e52ba6a112fc905e
Parrot5035 <wdrekas21@blakeschool.org>
2018-09-04 13:23:03 -0500

    Initial commit

---
+++ Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour {
+
+    //example comment
+    //hilight words and hit command ' to get linked to the API
+    //var example float int string boolean vector3(cords) game objects can also be var
+    public float rotateSpeed;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        transform.Rotate
+
+	}
+}
