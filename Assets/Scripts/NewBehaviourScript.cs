45bd3ec682bcead3b4fa00e390379f943772e90c
Parrot5035 <wdrekas21@blakeschool.org>
2018-09-07 13:55:49 -0500

    Got everything working and recovered

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.Translate(Vector3.up*Time.deltaTime);
+
+        if (transform.position.y > 8)
+            transform.localPosition += new Vector3(0,-8,0);
+
+
+    
+    }
+}
